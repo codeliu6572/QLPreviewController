43966be42f71da67a84e4b291ab4b954a174a13b
codeliu6572 <codeliu6572@163.com>
2016-03-29 09:46:46 +0800

    Initial Commit

---
+++ QLPreviewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QLPreviewController
+//
+//  Created by 刘浩浩 on 16/3/29.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QLPreviewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QLPreviewController
+//
+//  Created by 刘浩浩 on 16/3/29.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
